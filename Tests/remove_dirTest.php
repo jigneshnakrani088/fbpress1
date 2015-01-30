6fe01d576376d73702b5d083e4676f13fbe9a04d
jigneshnakrani088 <jignesh.nakrani@ymail.com>
2015-01-30 14:28:19 +0530

    PHPUnit test

--- Tests/remove_dirTest.php
+++ Tests/remove_dirTest.php
@@ -1,23 +1,26 @@
-
 <?php
 
 class remove_dirTest extends PHPUnit_Framework_TestCase {
 
     protected $remove_dir;
-
-    protected function setUp() {
+    function testrrmdir()
+	{
+		$directory = rand(111,11111);
+		//-- create dummy directory with a file 
+		try{
+			mkdir($directory);
+			$a=$directory."/a.txt";
+			$f=fopen($a,"w");
+			fclose($f);
+		}catch(Exception $e)
+		{
+			echo "Exist";
+		}
+		//-- act to remove dir
         $this->remove_dir = new remove_dir();
+    	$actual = $this->remove_dir->rrmdir( $directory );
+       	$this->assertEquals( $actual, $actual );
     }
-
-    function testrrmdir( $dir = null ) {
-        $actual = $this->remove_dir->rrmdir( $directory );
-        $this->assertEquals( $actual, $actual );
-    }
-
-    protected function tearDown() {
-        unset( $this->remove_dir );
-    }
-
 }
 
 ?>
