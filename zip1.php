452805e7337f0cdee74ff162314108c473d4787c
Jignesh Nakrani <jignesh.nakrani@ymail.com>
2015-04-08 11:10:54 +0530

    update

--- zip1.php
+++ zip1.php
@@ -3,8 +3,8 @@ error_reporting(0);
 require_once 'lib/facebook/facebook.php';
 require_once'remove_dir.php';
 $facebook = new facebook(array(						 //	JigsApp								demo
-	'appId'  => '1523273831269396',   				 // 815325925197979  					1523273831269396
-	'secret' => '05e1d394341cb408eda496cd4d174a4f',  // e6dcecf8b128ca7dcbd5b2a3797c0661   	05e1d394341cb408eda496cd4d174a4f
+	'appId'  => '815325925197979',   				 // 815325925197979  					1523273831269396
+	'secret' => 'e6dcecf8b128ca7dcbd5b2a3797c0661',  // e6dcecf8b128ca7dcbd5b2a3797c0661   	05e1d394341cb408eda496cd4d174a4f
     'cookie'    => true,
 ));
 
