179c5f75ae16e9845631f14825afb5294fa4a4a6
Jignesh Nakrani <jignesh.nakrani@ymail.com>
2015-04-08 10:57:08 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,56 +30,7 @@ function slideShow(albumId){
 											 $(form).submit();
 											});
 }
-/*function zipit(responce){
-	if ( responce != false )
-	{
-		$('#progress-bar').show();
-		var n=Math.round(100/responce.length);
-		$('#link').hide();
-		$('html, body').animate({ scrollTop: $('#profile_head').offset().top }, 'slow');
-		if(responce.length !==1) { $('.progress-bar').width(10).text(10);}
-		else { $('.progress-bar').width(11).text(11);}
-		var val=0;
-		FB.api('/me', function(res) {
-			var data = {'album_id': res.name,}
-			$.post('remove_zip.php', data , function(){
-					$.each( responce, function( index, value ){						
-						FB.api("/"+ value +"/photos", function (ph1) {
-								var para= new Array();
-								$.each(ph1.data, function(key, value1) {para.push(value1.source);});		
-								var data1={ 'source': para,'user_id':res.name,'album_id':value}
-								 $.ajax({
-										url: "zip1.php",
-										data: {'source' : para,'user_id':res.name,'album_id':value},
-										type: "POST",
-										async: false,
-										success: function(response)
-										{
-											if ( responce.length-1 == index )
-											{
-												val=val+n;
-												$('.progress-bar').width(val + '%').text(val + '%');
-												setTimeout(function(){  $('#progress-bar').hide();}, 200);
-												$('#link').show(400);
-												var res1 = jQuery.parseJSON(response);
-												$('#link').html("<h4><a href='"+ res1.url +"'>click here for download..</a></h4>");
-												$('#link').click(function(){$('#link').hide(600);$('#progress-bar').hide();});
-											}
-											else
-											{ 	
-												val=val+n;
-												$('.progress-bar').width(val + '%').text(val + '%');
-											}								
-										}
-								});
-						});
-					});
-			});
-		});
-	}
-}
 
-*/
 
 function zipit1(responce){
 	$('#link').hide();
@@ -106,55 +57,7 @@ function zipit1(responce){
 										 });
 	});
 }
-/*function zipit(responce){
-	if ( responce != false ){
-		$('#progress-bar').show();
-		var n=Math.round(100/responce.length);
-		$('#link').hide();
-		$('html, body').animate({ scrollTop: $('#profile_head').offset().top }, 'slow');
-		
-		if(responce.length !==1) { $('.progress-bar').width(1).text(1);}
-		else { $('.progress-bar').width(11).text(11);}
-		FB.api('/me', function(user) {
-							   var data1 = {'album_id': user.name,}
-		var val=0;
-		$.post('remove_zip.php', data1 , function(){
-			if(true){
-			 $.each( responce, function( index, value ){
-					FB.api('/'+value, function(u) {var a_name=u.name;
-						var data = {'zip_name':user.name,
-						'album_id': value,
-						'album_name':a_name, }
-					
-				$.post('zip1.php',data, function(response){
-						 
-							if ( responce.length-1 == index ){
-								val=val+n;
-								$('.progress-bar').width(val + '%').text(val + '%');
-								setTimeout(function(){  $('#progress-bar').hide();}, 100);
-								var res = jQuery.parseJSON(response);
-								if(res.status==false){
-									alert("Opps Something went wrong sorry..");	
-								}else{
-									$('#link').show(400);
-									var res1 = jQuery.parseJSON(response);
-									$('#link').html("<h4><a href='"+ res1.url +"'>click here for download..</a></h4>");
-									$('#link').click(function(){$('#link').hide(600);$('#progress-bar').hide();});
-								}
-								
-							}else{
-								val=val+n;
-								 $('.progress-bar').width(val + '%').text(val + '%');
-							}
-				})
-	 } );		});
-		 }
-		});										
-		
-		});
-	}
-}
-*/
+
 function CheckForm(){
 	var selectedcheckbox = [];
 	$.each($("input[name='checkalbum[]']:checked"), function(){            
@@ -240,7 +143,7 @@ $(document).ready(function() {
 				$.each($("input[name='checkalbum[]']"), function(){            
 					allcheckbox.push($(this).val());
 				});
-				zipit(allcheckbox);
+				zipit1(allcheckbox);
 			});
 	
 		$('#move_all').click(function(){
